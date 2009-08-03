6651e22a1d669e6bb122e2cdb801fc0169217922
Josh French <josh@digitalpulp.com>
2009-08-03 12:42:21 -0400

    Upgrade to new Integrity::Notifier::Test api

--- test/campfire_test.rb
+++ test/campfire_test.rb
@@ -1,9 +1,12 @@
 require File.dirname(__FILE__) + "/helper"
 
-context "The Campfire notifier" do
-  setup do
-    setup_database
 
+class IntegrityCampfireTest < Test::Unit::TestCase
+  include RR::Adapters::TestUnit
+  include Integrity::Notifier::Test
+
+  def setup
+    setup_database
     @config = { "account" => "integrity",
       "use_ssl" => false,
       "room"    => "ci",
@@ -11,65 +14,63 @@ context "The Campfire notifier" do
       "pass"    => "bar",
       "announce_success" => true }
     @notifier = Integrity::Notifier::Campfire
-    @room = stub(:speak => nil, :paste => nil, :leave => nil)
+    @campfire = Tinder::Campfire.new(@config['account'])
+    @room = Tinder::Room.new(@campfire, 12345)
   end
 
   def notifier
     "Campfire"
   end
 
-  test "it registers itself" do
-    assert_equal @notifier, Integrity::Notifier.available["Campfire"]
+  def test_registration
+    assert_equal Integrity::Notifier::Campfire, Integrity::Notifier.available["Campfire"]
   end
 
-  test "configuration form" do
-    assert_form_have_option "account", @config["account"]
-    assert_form_have_option "use_ssl", @config["use_ssl"]
-    assert_form_have_option "room",    @config["room"]
-    assert_form_have_option "user",    @config["user"]
-    assert_form_have_option "pass",    @config["pass"]
-    assert_form_have_option "announce_success", @config["announce_success"]
+  def test_configuration_form
+    assert provides_option?("account", @config["account"])
+    assert provides_option?("use_ssl", @config["use_ssl"])
+    assert provides_option?("room",    @config["room"])
+    assert provides_option?("user",    @config["user"])
+    assert provides_option?("pass",    @config["pass"])
+    assert provides_option?("announce_success", 1)
   end
 
-  test "ssl" do
-    @config["use_ssl"] = true
+  def test_should_use_ssl_if_configured
+    stub(@campfire) do |expect|
+      expect.login(@config['user'], @config['pass']) { true }
+      expect.find_room_by_name(@config['room']) { @room }
+    end
+    mock(Tinder::Campfire).new(@config['account'], {:ssl => true}) { @campfire }
 
-    Tinder::Campfire.expects(:new).with(@config["account"], { :ssl => true }).
-      returns(stub(:login => true, :find_room_by_name => @room))
+    @config['use_ssl'] = true
+    @notifier.notify_of_build(Integrity::Build.gen, @config)
+  end
+
+  def test_should_speak_successful_build
+    stub.instance_of(@notifier).room { @room }
+
+    dont_allow(@room).paste
+    mock(@room).speak(satisfy {|msg| msg =~ /was successful/})
 
     @notifier.notify_of_build(Integrity::Build.gen, @config)
   end
 
-  test "successful build" do
-    build = Integrity::Build.gen(:successful)
+  def test_should_paste_failed_build
+    stub.instance_of(@notifier).room { @room }
 
-    @notifier.any_instance.stubs(:room).at_least_once.returns(@room)
-    @room.expects(:speak).with { |value| value.include?(build.commit.identifier) }
-    @room.expects(:paste).never
+    mock(@room).speak(satisfy {|msg| msg =~ /failed/})
+    mock(@room).paste(satisfy {|msg| msg =~ /#{build.commit.message}/ })
 
-    @notifier.notify_of_build(build, @config)
+    @notifier.notify_of_build(Integrity::Build.gen(:failed), @config)
   end
 
-  test "don't announce successes" do
-    build = Integrity::Build.gen(:successful)
+  def test_should_not_announce_successful_builds_when_disabled
+    stub.instance_of(@notifier).room { @room }
+
+    dont_allow(@room).speak
+    dont_allow(@room).paste
 
     @config['announce_success'] = false
-    @notifier.any_instance.stubs(:room).at_least_once.returns(@room)
-    @room.expects(:speak).never
-
-    @notifier.notify_of_build(build, @config)
-  end
-
-  test "failed build" do
-    build = Integrity::Build.gen(:failed)
-
-    @notifier.any_instance.stubs(:room).at_least_once.returns(@room)
-    @room.expects(:speak).with { |value| value.include?(build.commit.identifier) }
-    @room.expects(:paste).with { |value|
-      value.include?(build.commit.message) &&
-        value.include?(build.output)
-    }
-
-    @notifier.notify_of_build(build, @config)
+    @notifier.notify_of_build(Integrity::Build.gen, @config)
   end
 end
--- test/helper.rb
+++ test/helper.rb
@@ -1,38 +1,11 @@
-require "test/unit"
-require "mocha"
-require "integrity/notifier/test"
-require File.dirname(__FILE__) + "/../lib/integrity/notifier/campfire"
+require 'test/unit'
+require 'rr'
+require 'integrity/notifier/test'
 
-begin
-  require "redgreen"
-rescue LoadError
-end
+require File.dirname(__FILE__) + '/../lib/integrity/notifier/campfire'
 
-class Test::Unit::TestCase
-  include Integrity::Notifier::Test
-end
-
-# NOTE: Because of a bug in integrity/notifier/test/fixtures
 class Array
   def pick
     slice(Kernel.rand(size))
   end
-end
-
-##
-# test/spec/mini 2
-# http://gist.github.com/25455
-# chris@ozmm.org
-#
-def context(*args, &block)
-  return super unless (name = args.first) && block
-  klass = Class.new(defined?(ActiveSupport::TestCase) ? ActiveSupport::TestCase : Test::Unit::TestCase) do
-    def self.test(name, &block)
-      define_method("test_#{name.gsub(/\W/,"_")}", &block) if block
-    end
-    def self.xtest(*args) end
-    def self.setup(&block) define_method(:setup, &block) end
-    def self.teardown(&block) define_method(:teardown, &block) end
-  end
-  klass.class_eval &block
 end
